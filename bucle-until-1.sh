461979c6a8a29f417c3dd7890d890691835b189d
DaviDSanchezN <158029135+DaviDSanchezN@users.noreply.github.com>
2024-02-04 20:28:51 +0100

    Update bucle-until-1.sh

--- bucle-until-1.sh
+++ bucle-until-1.sh
@@ -4,8 +4,9 @@
 
 # Utiliza un bucle until para comprobar si el número de parámetros es igual a 3
 until [ "$#" -eq 3 ]; do
-    echo "Error: S'esperaven 3 paràmetres. Proporciona exactament 3 valors."
-    read -p "Introdueix 3 valors: " valor1 valor2 valor3
+    echo "Error: Se esperaven 3 paràmetres. Proporciona exactament 3 valors."
+    read -p "Introdueix 3 valors: " valores
+    set -- $valores
 done
 
 # Si se han proporcionado tres parámetros, mostrarlos
